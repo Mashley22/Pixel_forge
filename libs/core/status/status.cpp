96f4c71de0e197cf5ebc7dcb8321484e888941fe
Mashley22 <ajkhsu22@gmail.com>
2026-01-09 23:04:42 +0000

    Basic stub and implementation for the status thing

---
+++ libs/core/status/status.cpp
@@ -0,0 +1,30 @@
+module PixelForge.core.status;
+
+import PixelForge.core.assert;
+
+#include <atomic>
+#include <cstddef>
+#include <new>
+
+namespace pf {
+
+// ugh no anonymous structs ...
+struct alignas(std::hardware_destructive_interference_size) PaddedStatus {
+  std::atomic<Status> status;
+  std::byte __padding[std::hardware_destructive_interference_size - sizeof(Status)];
+};
+
+static_assert(sizeof(PaddedStatus) == std::hardware_destructive_interference_size);
+
+namespace {
+
+PaddedStatus M_paddedStatus{ .status = Status::OK };
+
+}
+
+std::atomic<Status>&
+loadStatus(void) {
+  return M_paddedStatus.status;
+}
+
+}
---
+++ libs/core/status/status.cppm
@@ -0,0 +1,25 @@
+module;
+
+#include <atomic>
+#include <cstdint>
+
+#include <PixelForge/core/macros.hpp>
+
+export module PixelForge.core.status;
+
+namespace pf {
+
+using StatusUnderlying_t = std::uint64_t;
+
+export
+enum Status : StatusUnderlying_t {
+  OK,
+  TERMINATE,
+  EXCEPT
+}
+
+export [[nodiscard]]
+std::atomic<Status>&
+currentStatus(void) PF_NOEXCEPT;
+
+}
