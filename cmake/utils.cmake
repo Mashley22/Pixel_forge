19f9f6d127f2f9b8dbaeec00b2a8b0a403488673
Ashley <ajkhsu22@gmail.com>
2026-01-13 23:58:52 +0000

    Moved the pedantic compile options into cmake so that it can be target specific to avoid forcing these on the dependencies

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -27,3 +27,25 @@ function(target_cxx_modules_private TARGET_NAME)
   m_target_cxx_modules(${TARGET_NAME} PRIVATE private_cxx_modules ${MODULE_FILES})
 
 endfunction()
+
+set(PEDANTIC_COMPILE_OPTIONS
+  -Wall 
+  -Wextra 
+  -Wpedantic 
+  -Werror 
+  -Wshadow 
+  -Wconversion 
+  -Wsign-conversion 
+  -Wcast-align 
+  -Wcast-qual 
+  -Wformat=2 
+  -Wundef 
+  -Wnull-dereference 
+  -Wdouble-promotion 
+  -Wimplicit-fallthrough 
+  -Wmissing-declarations 
+  -Wredundant-decls 
+  -Wstrict-overflow=5 
+  -Wswitch-enum 
+  -Wunreachable-code
+)
