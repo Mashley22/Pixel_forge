2d8e70e9e97df9b95b4f128e09ef5c9fe731bc53
Ashley <ajkhsu22@gmail.com>
2026-01-14 00:26:09 +0000

    Working and complete unittest for the require functionality

--- validation/core/require/test_require.cpp
+++ validation/core/require/test_require.cpp
@@ -1,9 +1,47 @@
+#include <source_location>
+
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
 
 import PixelForge.core.require;
 
 namespace pf {
-  TEST_CASE( "basic", "[core][assert]") {
-    REQUIRE_NOTHROW(require(true));
+
+namespace {
+
+void
+M_testRequireInfos(void) {
+  std::string_view msg = "funny message here";
+  try {
+    require(false, msg);
+    REQUIRE_FALSE(true);
   }
+  catch(RequireFail& e) {
+    REQUIRE(e.msg == msg);
+    // a lil bit of magic
+    REQUIRE_THAT(e.loc.file_name(), Catch::Matchers::ContainsSubstring("test_require.cpp"));
+    REQUIRE(e.loc.line() == 16);
+    REQUIRE_THAT(e.loc.function_name(), Catch::Matchers::Equals("void pf::(anonymous namespace)::M_testRequireInfos()")); // A bit unsure how guaranteed this is
+    REQUIRE(e.loc.column() == 5);
+  }
+}
+
+}
+
+TEST_CASE( "basic", "[core][assert]") {
+  REQUIRE_NOTHROW(require(true));
+
+  try {
+    require(false);
+    REQUIRE_FALSE(true);
+  }
+  catch(RequireFail& e) {
+    (void)e;
+  }
+}
+
+TEST_CASE( "correct infos", "[core][assert]") {
+  M_testRequireInfos();
+}
+
 }
