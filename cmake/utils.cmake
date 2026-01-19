62ae28504b940a1714a205a867027c9559998bc5
Ashley <ajkhsu22@gmail.com>
2026-01-19 12:54:34 +0000

    Fixed issues with passing all of the ARGN s correctly

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -16,15 +16,15 @@ function(target_cxx_modules_public TARGET_NAME)
 
   set(MODULE_FILES ${ARGN})
     
-  m_target_cxx_modules(${TARGET_NAME} "PUBLIC" "public_cxx_modules" ${MODULE_FILES})
+  m_target_cxx_modules(${TARGET_NAME} "PUBLIC" "public_cxx_modules" "${MODULE_FILES}")
 
 endfunction()
 
 function(target_cxx_modules_private TARGET_NAME)
 
-  set(MODULE_FILES ${ARG_UNPARSED_ARGUMENTS})
+  set(MODULE_FILES ${ARGN})
 
-  m_target_cxx_modules(${TARGET_NAME} PRIVATE private_cxx_modules ${MODULE_FILES})
+  m_target_cxx_modules(${TARGET_NAME} PRIVATE private_cxx_modules "${MODULE_FILES}")
 
 endfunction()
 
