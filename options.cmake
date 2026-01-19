f260bff8801854a1361841d4c287e7c7af566923
Ashley <ajkhsu22@gmail.com>
2026-01-19 14:23:51 +0000

    Fix to add the compile_definitions before adding any sources so they can all see the compile_definition, and in optimised build the memtest_explicit tests no longer try to compile with a range of optimisation levels

--- options.cmake
+++ options.cmake
@@ -1,1 +1,28 @@
 option(PIXELFORGE_TEST OFF "Test build")
+
+option(PIXELFORGE_AGGRESSIVE_OPTIMISATIONS "Enable -O3 with LTO" OFF)
+
+if(PIXELFORGE_TEST)
+  add_compile_definitions(PIXELFORGE_TEST)
+endif()
+
+if(PIXELFORGE_AGGRESSIVE_OPTIMISATIONS)
+  add_compile_definitions(PIXELFORGE_AGGRESSIVE_OPTIMISATIONS)
+
+  include(CheckIPOSupported)
+  check_ipo_supported(RESULT supported OUTPUT error)
+
+  if(supported)
+    message(STATUS "IPO / LTO enabled")
+    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
+
+  else()
+    message(STATUS "IPO / LTO not supported: <${error}>")
+  endif()
+
+  if(MSVC)
+    add_compile_options("/Ox")
+  else()
+    add_compile_options("-O3")
+  endif()
+endif()
