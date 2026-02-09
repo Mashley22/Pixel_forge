c90be2f92506c7d5b621e8d4e660caf45fcc1806
Ashley <ajkhsu22@gmail.com>
2026-02-09 19:25:58 +0000

    Added release mode to the optimised unit tests

--- build/validation/optimised/build.sh
+++ build/validation/optimised/build.sh
@@ -2,4 +2,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo $SCRIPT_DIR
 source $SCRIPT_DIR/../../build_utils.sh
 
-eval cmake ../../../ -G Ninja -DPIXELFORGE_AGGRESSIVE_OPTIMISATIONS=ON "$@" -DPIXELFORGE_TEST=ON
+eval cmake ../../../ -G Ninja -DPIXELFORGE_AGGRESSIVE_OPTIMISATIONS=ON "$@" -DPIXELFORGE_TEST=ON -DCMAKE_BUILD_TYPE=Release
