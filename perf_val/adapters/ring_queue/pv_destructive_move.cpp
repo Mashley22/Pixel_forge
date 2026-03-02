60c066f4c9d19b3d6271590e89fb50110af6a02f
Ashley <ajkhsu22@gmail.com>
2026-03-02 21:58:31 +0000

    Pulling in the new variance of mean stuff from benchpp and updating the perf_val so the tests loops are cache line aligned and not inlined.

--- perf_val/adapters/ring_queue/pv_destructive_move.cpp
+++ perf_val/adapters/ring_queue/pv_destructive_move.cpp
@@ -2,7 +2,9 @@ import Benchpp;
 import PixelForge.adapters.ringQueue;
 
 #include <array>
+#include <cmath>
 #include <iostream>
+#include <new>
 
 #include <catch2/catch_all.hpp>
 
@@ -10,6 +12,8 @@ import PixelForge.adapters.ringQueue;
 #define RUN_NUM 10000
 #define RUN_SIZE 10000
 #define TOLERANCE 1.05
+
+#define ATTRIB_CACHE_LINE_ALIGN __attribute__((aligned(std::hardware_destructive_interference_size), noinline))
 
 namespace pf::adapters {
 
@@ -31,11 +35,7 @@ public:
     return *this;
   }
 
-  ~NoOpDestructor() {
-    if (x != nullptr) {
-     x = nullptr;
-    }
-  }
+  ~NoOpDestructor() = default;
 };
 
 class NoOpAfterMoveDestructor {
@@ -62,6 +62,32 @@ public:
   }
 };
 
+benchpp::Timer noOpTimer;
+benchpp::Timer noOpAfterMoveTimer;
+
+ATTRIB_CACHE_LINE_ALIGN
+void foo(RingQueue<NoOpDestructor>& noOp) {
+  noOpTimer.start();
+  for (std::size_t j = 0; j < RUN_SIZE; j++) {
+    noOp.emplace_unchecked();
+    auto x = noOp.pop_unchecked();
+    x.x = nullptr;
+  }
+  noOpTimer.stop();
+  noOpTimer.recordAndReset();
+}
+
+ATTRIB_CACHE_LINE_ALIGN
+void foo2(RingQueue<NoOpAfterMoveDestructor>& noOpAfterMove) {
+  noOpAfterMoveTimer.start();
+  for (std::size_t j = 0; j < RUN_SIZE; j++) {
+    noOpAfterMove.emplace_unchecked();
+    auto x = noOpAfterMove.pop_unchecked();
+  }
+  noOpAfterMoveTimer.stop();
+  noOpAfterMoveTimer.recordAndReset();
+}
+
 static_assert(sizeof(NoOpDestructor) == sizeof(NoOpAfterMoveDestructor));
 static_assert(sizeof(NoOpDestructor) == sizeof(void*));
 
@@ -73,36 +99,18 @@ TEST_CASE( "validate destructor is not called if no op", "[adapters][RingQueue]"
   RingQueue<NoOpDestructor> noOp(reinterpret_cast<NoOpDestructor*>(noOpBuf.data()), BUF_SIZE);
   RingQueue<NoOpAfterMoveDestructor> noOpAfterMove(reinterpret_cast<NoOpAfterMoveDestructor*>(otherBuf.data()), BUF_SIZE);
 
-  benchpp::Timer noOpTimer;
-  benchpp::Timer noOpAfterMoveTimer;
-
   for (std::size_t i = 0; i < RUN_NUM; i++) {
-    noOpTimer.start();
-    for (std::size_t j = 0; j < RUN_SIZE; j++) {
-      noOp.emplace_unchecked();
-      auto x = noOp.pop_unchecked();
-      x.x = nullptr;
-    }
-    noOpTimer.stop();
-    noOpTimer.recordAndReset();
-
-    noOpAfterMoveTimer.start();
-    for (std::size_t j = 0; j < RUN_SIZE; j++) {
-      noOpAfterMove.emplace_unchecked();
-      auto x = noOpAfterMove.pop_unchecked();
-    }
-    noOpAfterMoveTimer.stop();
-    noOpAfterMoveTimer.recordAndReset();
-
+    foo2(noOpAfterMove);
+    foo(noOp);
   }
   
   auto noOpStats = benchpp::Stats<benchpp::TimeCount_t>::generate(noOpTimer.times());
   auto otherStats = benchpp::Stats<benchpp::TimeCount_t>::generate(noOpAfterMoveTimer.times());
+  
+  double combinedStddevOfMean = std::sqrt(noOpStats.varianceOfMean() + otherStats.varianceOfMean());
 
-  std::cout << otherStats.mean << '\n';
-  std::cout << noOpStats.mean << '\n';
-  REQUIRE_FALSE(true);
-  REQUIRE(otherStats.mean < noOpStats.mean);
+  REQUIRE_THAT(noOpStats.mean, Catch::Matchers::WithinAbs(otherStats.mean, combinedStddevOfMean));
+  
 }
 
 }
